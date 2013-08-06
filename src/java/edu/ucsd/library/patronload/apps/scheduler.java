a494a90913989a6a1463dc064f90a98a494b1a42
VivianChu <tchu@ucsd.edu>
2013-08-06 11:58:07 -0700

    Changing the schedule time for autorun from 3am to 3:30am

--- src/java/edu/ucsd/library/patronload/apps/scheduler.java
+++ src/java/edu/ucsd/library/patronload/apps/scheduler.java
@@ -100,7 +100,7 @@ public class scheduler extends HttpServlet {
 	public void runme(String contextDir) {
 		Calendar calendar = Calendar.getInstance();
 		calendar.set(Calendar.HOUR_OF_DAY, 3);
-		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.MINUTE, 30);
 		calendar.set(Calendar.SECOND, 0);
 		Date time = calendar.getTime();
 
