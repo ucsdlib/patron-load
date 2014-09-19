a233b9f3bf435e6c33cd3227ff43488fc96e1d8f
VivianChu <tchu@ucsd.edu>
2014-09-19 16:02:24 -0700

    Updating the code for testing. PATRONLOADKEY-8

--- src/java/edu/ucsd/library/patronload/apps/scheduler.java
+++ src/java/edu/ucsd/library/patronload/apps/scheduler.java
@@ -60,9 +60,9 @@ public class scheduler extends HttpServlet {
 		    fullquery.setMarcFilesDir(marcFilesDir);	
 		    System.out.println(
 			"***Patronload Scheduler Status: running Date" + new java.util.Date());
-			edu.ucsd.library.patronload.apps.doinc.main(tmp);
+			//edu.ucsd.library.patronload.apps.doinc.main(tmp);
 			// generate the incremental employee marc file 
-			edu.ucsd.library.patronload.apps.doinc_employee.main(tmp);
+			//edu.ucsd.library.patronload.apps.doinc_employee.main(tmp);
 			System.out.println("***Patronload Scheduler Status: Done Date" + new java.util.Date());
 		}
 
