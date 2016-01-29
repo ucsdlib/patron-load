852a0518dd16357ba234d257fb47dd839606584c
VivianChu <tchu@ucsd.edu>
2016-01-29 10:52:54 -0800

    Update the patron properties download output. PL-7

--- src/java/edu/ucsd/library/patronload/apps/downloadProperties.java
+++ src/java/edu/ucsd/library/patronload/apps/downloadProperties.java
@@ -79,8 +79,10 @@ public void doGet(HttpServletRequest request, HttpServletResponse response) thro
 	 while(it.hasNext())
 	 {
 		 Map.Entry pairs = (Map.Entry)it.next();
-		 String temp2 = pairs.getKey().toString() + "=" + pairs.getValue() + "\n";
-		 os.write(temp2.getBytes());
+		 if(pairs.getKey().toString().length() > 0) {
+			String temp2 = pairs.getKey().toString() + "=" + pairs.getValue() + "\n";
+		 	os.write(temp2.getBytes());
+		 }
 	 }
 	 
 	 os.flush();  
