04d313d95eeb2558c6d2d9d9e814ffa89446d3eb
VivianChu <tchu@ucsd.edu>
2016-01-29 10:07:49 -0800

    Update the download and edit properties files feature. PL-7

--- src/java/edu/ucsd/library/patronload/apps/downloadProperties.java
+++ src/java/edu/ucsd/library/patronload/apps/downloadProperties.java
@@ -12,6 +12,7 @@ import java.util.*;
 import java.util.Map;
 import java.util.TreeMap;
 import java.util.SortedMap;
+import edu.ucsd.library.util.FileUtils;
 
 public class downloadProperties extends HttpServlet {  
    
@@ -30,54 +31,57 @@ public void doGet(HttpServletRequest request, HttpServletResponse response) thro
 	 response.setContentType("text/plain"); 
 	 response.setHeader("Content-Disposition","attachment;filename=" + fileName);
 	 ServletContext ctx = getServletContext();
-	 String marcFilesDir = ctx.getInitParameter("marcFilePath");
-	 boolean readPatronFile = false;
-	 
+	 String marcFilesDir = ctx.getInitParameter("marcFilePath");	 
 	 BufferedReader is = new BufferedReader(new FileReader(marcFilesDir + fileName));
 	 is.readLine();
 	 is.readLine();
 	 is.readLine();
-	 if(fileName.equals("patron_load.properties")) {
-		 readPatronFile = true;
-		 is.readLine();
-	 }
 	 if(fileName.equals("emp_affiliations.properties"))
 		 is.readLine();
 	 String lineIn;
-	 ServletOutputStream os = response.getOutputStream();  
-	 Map<Integer, String> propMap = new HashMap<Integer, String>();
-	 Map<String, String> patronMap = new HashMap<String, String>();
-	 
-	 if (readPatronFile) {
-		 String inTmp = "";
-		 while((lineIn = is.readLine()) != null)
-		 {
-			 inTmp = lineIn + "\n";
-			 if(!lineIn.contains("db") && !lineIn.contains("expire"))
-				 os.write(inTmp.getBytes());
-		 }		 
-	 } else {
-		 while((lineIn = is.readLine()) != null)
-		 {
-			 String[] temp = lineIn.split("=");
-			 if(temp.length == 2)
-				 propMap.put(Integer.parseInt(temp[0]),temp[1]);
-			 else
-				 propMap.put(Integer.parseInt(temp[0]), "");
+	 ServletOutputStream os = response.getOutputStream();  	 
+	 Map<String,String> sortedMap = null;	 
+     if(fileName.equals("patron_load.properties"))
+     {
+    	 	Map<String, String> propPatronMap = new HashMap<String, String>();
+    	 	Properties props = null;
+        try {
+            	props = FileUtils.loadProperties(marcFilesDir + fileName);
+        } catch (IOException ioe) {
+        		System.err.println("IOException:"+ioe);
+        } 
+    	 	Enumeration e = props.propertyNames();
+	    for (; e.hasMoreElements(); ) {
+	        String propName = (String)e.nextElement();
+	        String propValue = (String)props.get(propName);
+	        if(propValue.length() < 5 && !propName.contains("quartercode"))
+	        		propPatronMap.put(propName, propValue);
+	    }    
+	    sortedMap = new TreeMap(propPatronMap);
+     } else {
+    	 	Map<Integer, String> propMap = new HashMap<Integer, String>();
+		while((lineIn = is.readLine()) != null)
+		{
+			String[] temp = lineIn.split("=");
+			if(temp.length == 2)
+				propMap.put(Integer.parseInt(temp[0]),temp[1]);
+			else
+				propMap.put(Integer.parseInt(temp[0]), "");
 		 }
 		 if(fileName.equals("emp_affiliations.properties"))
 			 os.write("##Employee Download - Affiliation code\n#[department code] = [library code]\n".getBytes());
 		 else
 			 os.write("##Staff Group\n".getBytes());
-		 Map<Integer, String> sortedMap = new TreeMap(propMap);
-		 Iterator it = sortedMap.entrySet().iterator();
-		 while(it.hasNext())
-		 {
-			 Map.Entry pairs = (Map.Entry)it.next();
-			 String temp2 = pairs.getKey().toString() + "=" + pairs.getValue() + "\n";
-			 os.write(temp2.getBytes());
-		 }
+     }
+		 
+	 Iterator it = sortedMap.entrySet().iterator();
+	 while(it.hasNext())
+	 {
+		 Map.Entry pairs = (Map.Entry)it.next();
+		 String temp2 = pairs.getKey().toString() + "=" + pairs.getValue() + "\n";
+		 os.write(temp2.getBytes());
 	 }
+	 
 	 os.flush();  
 	 os.close(); 
 	}  
